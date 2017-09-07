1a2ed3fdff6f26f5c8404240c610c8ce21da897a
Wagner Almeida <wdmeida@gmail.com>
2017-09-07 20:07:34 -0300

    Refactoring login form from main domain.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
     def layout_by_resource
       if devise_controller? && resource_name == :admin
         "backoffice_devise"
+      elsif devise_controller? && resource_name == :secretary
+        "upa_devise"
       else
         "application"
       end 
