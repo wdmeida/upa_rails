c27b6e5ac6541bec53e32238663517741775ffd6
Wagner Almeida <wagner.almeida@inovamind.com.br>
2020-10-13 21:16:53 -0300

    Update data

---
+++ public/assets/forms-f9951b0f9dad321fc07ebd33e0b9a810b962fcd13a44397b8d64a835ebbef350.js
@@ -0,0 +1,13 @@
+const validationFormSubmit = function (idForm) {
+  "use strict";
+  window.addEventListener("load", function() {
+    var form = document.getElementById(idForm);
+    form.addEventListener("submit", function(event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add("was-validated");
+    }, false);
+  }, false);
+};
